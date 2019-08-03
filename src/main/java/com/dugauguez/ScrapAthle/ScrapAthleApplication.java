7a61178575086daef5135fe04c1f24f336e1182f
GeorgesD <georges-xavier.degauquier@ineat-conseil.fr>
2019-08-03 22:18:07 +0200

    feat() : first commit for the java version

---
+++ src/main/java/com/dugauguez/ScrapAthle/ScrapAthleApplication.java
@@ -0,0 +1,13 @@
+package com.dugauguez.ScrapAthle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScrapAthleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScrapAthleApplication.class, args);
+	}
+
+}
