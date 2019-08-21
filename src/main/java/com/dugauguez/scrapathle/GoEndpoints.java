9cbf78d3edec8414ec766e2f4846c70fe3e6b477
Raouf25 <makhlouf.raouf@gmail.com>
2019-08-21 14:45:30 +0200

    Feat/documentation (#8)
    * doc(swagger): Add swagger Documentation Close#6
    
    * feat(): add banner file
    
    * chore(gitignore) update ignored files

---
+++ src/main/java/com/dugauguez/scrapathle/GoEndpoints.java
@@ -0,0 +1,17 @@
+package com.dugauguez.scrapathle;
+
+public abstract class GoEndpoints {
+
+    public static final String ROOT = "/";
+    public static final String SWAGGER = "/swagger";
+    public static final String SWAGGER_API_DOCS = "/v2/api-docs";
+    public static final String SWAGGER_WEBJARS = "/webjars/**";
+    public static final String SWAGGER_RESOURCES = "/swagger-resources/**";
+    public static final String SWAGGER_UI = "/swagger-ui.html";
+
+    public static final String DEPARTMENTS = "/departments";
+    public static final String FILES_YEAR = "/files/general/{year}";
+    public static final String SCRAPINGS_YEAR = "/scrapings/{year}";
+
+
+}
