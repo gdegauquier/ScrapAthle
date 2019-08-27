4bbe1980e78897d35f4e51746b61bd367618f89a
Raouf25 <makhlouf.raouf@gmail.com>
2019-08-27 09:43:32 +0200

    feat() : parse addresses (#11)

---
+++ src/main/java/com/dugauguez/scrapathle/entity/Address.java
@@ -0,0 +1,44 @@
+package com.dugauguez.scrapathle.entity;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Data
+@Entity
+@Table(name = "address")
+public class Address {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @JsonProperty("name")
+    private String name = null;
+
+    @JsonProperty("Line1")
+    private String line1 = null;
+
+    @JsonProperty("Line2")
+    private String line2 = null;
+
+    @JsonProperty("Line3")
+    private String line3 = null;
+
+    @JsonProperty("Ville")
+    private String town = null;
+
+    @JsonProperty("Code Postal")
+    private String postalCode = null;
+
+    @JsonProperty("Type")
+    private String type = null;
+
+    private String phoneNumber1 = null;
+
+    private Integer eventId;
+
+    @JsonProperty("Code Postal")
+    private String codePostal = null;
+}
---
+++ src/main/java/com/dugauguez/scrapathle/repository/AddressRepository.java
@@ -0,0 +1,10 @@
+package com.dugauguez.scrapathle.repository;
+
+import com.dugauguez.scrapathle.entity.Address;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AddressRepository extends CrudRepository<Address, Integer> {
+
+}
